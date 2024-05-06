871d1115c5f3d422ce6145caf254dd2b6f4dfc76
Eryza22 <113457833+Eryza22@users.noreply.github.com>
2024-05-07 01:18:35 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// let typed = new Typed(".konten_beranda h3", {
+//   strings: ["kelompok ciko", "mbois ilakes"],
+//   typeSpeed: 60,
+//   backSpeed: 60,
+//   backDelay: 1000,
+//   loop: true,
+// });
+  
